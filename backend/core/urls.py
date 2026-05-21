321d51fc8a71129116453597c9e3bd25a0b51383
Diego Antonio Torres Moreno <147529728+DeftOZK@users.noreply.github.com>
2026-05-20 17:54:37 -0700

    Make routes runnable without app url modules

--- backend/core/urls.py
+++ backend/core/urls.py
@@ -1,16 +1,37 @@
 from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
-from django.urls import include, path
-from django.views.generic import TemplateView
+from django.http import HttpResponse
+from django.urls import path
+
+
+def home(request):
+    return HttpResponse('La Cabana Website - inicio')
+
+
+def menu(request):
+    return HttpResponse('Menu digital')
+
+
+def clientes(request):
+    return HttpResponse('Clientes')
+
+
+def puntos(request):
+    return HttpResponse('Sistema de puntos')
+
+
+def feedback(request):
+    return HttpResponse('Feedback')
+
 
 urlpatterns = [
-    path('', TemplateView.as_view(template_name='home.html'), name='home'),
+    path('', home, name='home'),
     path('admin/', admin.site.urls),
-    path('menu/', include('menu.urls')),
-    path('clientes/', include('clientes.urls')),
-    path('puntos/', include('puntos.urls')),
-    path('feedback/', include('feedback.urls')),
+    path('menu/', menu, name='menu_home'),
+    path('clientes/', clientes, name='clientes_home'),
+    path('puntos/', puntos, name='puntos_home'),
+    path('feedback/', feedback, name='feedback_home'),
 ]
 
 if settings.DEBUG:
